c4401e88de78241507adc97f9962d3753e920b1a
Jami <jamidefenbaugh@gmail.com>
2023-02-17 14:41:41 -0600

    from Thymeleaf lesson

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -8,34 +8,40 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
-/**
- * Created by Chris Bay
- */
 @Controller
 @RequestMapping("events")
 public class EventController {
 
-    private static List<String> events = new ArrayList<>();
+//    private static List<String> events = new ArrayList<>();
+
 
     @GetMapping
     public String displayAllEvents(Model model) {
-        model.addAttribute("title", "All Events");
+//        model.addAttribute("events", events);
+//        return "events/index";
+
+        HashMap<String, String> events = new HashMap<>();
+        events.put("Code. Now Code Again", "When one round of coding just isn't enough!");
+        events.put("Tired and Coding", "For pretend coders with day jobs!");
+        events.put("Eat...and Also Code", "For people who eat first, code second!");
+
         model.addAttribute("events", events);
         return "events/index";
     }
 
+    //lives at events/create
     @GetMapping("create")
-    public String displayCreateEventForm(Model model) {
-        model.addAttribute("title", "Create Event");
+    public String renderCreateEventForm() {
         return "events/create";
     }
 
-    @PostMapping("create")
-    public String processCreateEventForm(@RequestParam String eventName) {
-        events.add(eventName);
-        return "redirect:";
-    }
-
+    //also lives at events/create - this okay because it is post request, not get...otherwise could not do this
+//    @PostMapping("create")
+//    public String createEvent(@RequestParam String eventName) {
+////        events.add(eventName);
+////        return "redirect:"; //redirects to root path once added?
+//    }
 }
