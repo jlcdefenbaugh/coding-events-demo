55f9216c23ea46808370a58525619b4722d75fe8
Jami <jamidefenbaugh@gmail.com>
2023-03-22 15:48:15 -0500

    wrote code to register filter with Spring

---
+++ src/main/java/org/launchcode/codingevents/WebApplicationConfig.java
@@ -0,0 +1,23 @@
+package org.launchcode.codingevents;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+//annotation flags class to Spring, implement to ensure Spring calls AddInterceptors method during startup, register filter
+@Configuration
+public class WebApplicationConfig implements WebMvcConfigurer {
+
+    //Create spring-managed object to allow the app to access our filter
+    @Bean
+    public AuthenticationFilter authenticationFilter() {
+        return new AuthenticationFilter();
+    }
+
+    //Register the filter with the Spring container
+    @Override
+    public void addInterceptors(InterceptorRegistry register) {
+        register.addInterceptor(authenticationFilter());
+    }
+}
